888fcf05376e13e3dbd4e06d3179a8d2d50ac86d
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-10-15 00:25:09 +0800

    Update FedPD.sh

--- FedPD.sh
+++ FedPD.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 #SBATCH --account=def-arashmoh
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=4 # 8, 16
-#SBATCH --mem=32G               # memory per node (ex: 16G) you can get more 
-#SBATCH --time=01:00:00 		   # time period you need for your code (it is 12 hours for example)
-#SBATCH --mail-user=<jamal73sm@gmail.com> 	# replace with your email address to get emails to know when it is started or failed. 
+#SBATCH --gpus-per-node=a100:1       # Narval: a100, a100_4g.20g; Cedar: p100, p100l, v100l, a40
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1            # adjust (e.g., 8 or 16 if needed)
+#SBATCH --mem=40G                    # memory per node
+#SBATCH --time=2:00:00               # job time limit (HH:MM:SS)
+#SBATCH --mail-user=jamal73sm@gmail.com
 #SBATCH --mail-type=ALL
 
-#cd /home/shahab33/projects/def-arashmoh/shahab33/FeDK2P
-cd /project/def-arashmoh/shahab33/Rohollah/projects/FeDK2P/FeDK2P
+
+
+#cd /home/shahab33/projects/def-arashmoh/shahab33/GenFKD #Cedar
+
+#cd /project/def-arashmoh/shahab33/Rohollah/projects/FeD2P #Graham
+
+cd /project/def-arashmoh/shahab33/GenFKD #Narval 
 
 
 module purge
 module load python
 module load cuda
 
-#source ~/FeDK2P/bin/activate  	# activate your environment
-source /project/def-arashmoh/shahab33/Rohollah/projects/FeDK2P/FeDK2P/fedk2p/bin/activate 
 
-python fedk2p.py   	# this is the direction and the name of your code
+#source /home/shahab33/FeDK2P/bin/activate  	# Cedar
+
+#source /home/shahab33/fed2p/bin/activate #Graham
+
+source /home/shahab33/fed2p/bin/activate #Narval
+
+#python main1.py --local_model_name "ResNet20" --num_train_samples 10000 --alpha_dirichlet 10 --output_name "ResNet20_10K_alpha10_"  	# this is the direction and the name of your code
+#python main.py --local_model_name "ResNet18" --dataset "SVHN" --num_train_samples 11000 --alpha_dirichlet 10 --rounds 30 --num_synth_img_per_class 100 --output_name "_ResNet18_10K_alpha10_synth100_T4"
+#python main.py --local_model_name "ResNet18" --dataset "cifar10" --num_train_samples 33000 --alpha_dirichlet 10 --rounds 50 --num_synth_img_per_class 300 --output_name "_RN18_30K_alpha10_synth300_"
+python main.py 
+
+#python ImgGen.py
